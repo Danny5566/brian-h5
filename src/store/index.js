18d50d5708e1e6a4411947166ec4dfdbaf82d49a
iluchen <813502730@qq.com>
2019-12-18 17:32:56 +0800

    新增：vuex-persistedstate处理store中数据，保存到sessionStorage中防止刷新后store数据清空

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,8 @@ import Vuex from "vuex";
 import user from "./module/user";
 import app from "./module/app";
 
+import createPersistedState from "vuex-persistedstate";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -13,5 +15,10 @@ export default new Vuex.Store({
   modules: {
     user,
     app
-  }
+  },
+  plugins: [
+    createPersistedState({
+      storage: window.sessionStorage
+    })
+  ] // 默认将vuex数据保存到localStorage防止刷新丢失，修改为sessionStorage，页面关闭后清空
 });
